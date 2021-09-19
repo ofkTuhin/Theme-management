40161b082e6e97ed53cd209cf46782e7c0afb1df
omar-faruk02 <tuhin.omar99@gmail.com>
2021-09-19 18:21:26 +0600

    2nd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+ 
 } from "react-router-dom";
 import Home from './Component/Home/Home/Home';
 
