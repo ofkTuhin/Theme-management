385ff2c536e3a8b0c0774e48d4ba462e73ab7701
omar-faruk02 <tuhin.omar99@gmail.com>
2021-09-21 16:34:02 +0600

    commit

--- src/Component/Theme/ThemeFeture/ThemeFeature.js
+++ src/Component/Theme/ThemeFeture/ThemeFeature.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AddForm from '../../Home/AddForm/AddForm';
+
 import './themFetaure.css'
 
 const ThemeFeature = ({dataInput,deleteItem}) => {
