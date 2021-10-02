aedd8ba81323666a796ca4063dba8f58507a8841
omar-faruk02 <tuhin.omar99@gmail.com>
2021-10-02 15:34:09 +0600

    mmm

--- src/Component/Home/Home/Home.js
+++ src/Component/Home/Home/Home.js
@@ -1,40 +1,30 @@
 import axios from 'axios';
 import React from 'react';
 import AddForm from '../AddForm/AddForm';
-// import { Base64 } from 'js-base64';
-// import axios from 'axios';
-// import parse from 'html-react-parser';
 
-// import markdownPro from 'markdown-pro';
 
 
 const Home = () => {
     // const [encode, setEncode] = useState('')
-    const p='hi update ok'
+    const p={
+        a:'y'
+    }
 
 
+const postData=()=>{
+    axios.post('http://localhost:3000/updateTest',{
+        p
 
-// setInterval(()=>{
-//     axios.post('http://localhost:3000/updateTest',{
-//         p
-//         })
-//         console.log(p)
-// },[3000])
-const date=new Date('2021-10-02T05:59:57Z')
-console.log(date.getSeconds())
+    })
+}
+const q={
+    b:'z'
+}
+setInterval(()=>{
+    axios.patch('http://localhost:3000/update',{q})
+},[1000])
 
-
-    // useEffect(() => {
-    //     axios.get('https://api.github.com/repos/themefisher/vex-hugo/contents/README.md')
-    //         .then(data => setEncode(data.data.content))
-
-    // }, [])
-    // // console.log(encode)
-    // const decodeCode=Base64.atob(encode)
-    // // console.log(decodeCode)
-    // const parsCode =  markdownPro(decodeCode)
-    // console.log(parsCode)
-
+   
 
 
 
@@ -44,19 +34,11 @@ console.log(date.getSeconds())
             <h2 className="heading">Add Theme Description</h2>
             <AddForm></AddForm>
 
-            {/* <div style={{
-                border: '1px solid blue',
-                height: "300px",
-                width: "300px"
-
-            }}>
-
-                {parse(parsCode)}
-
-            </div> */}
+           
             <div>
 
             </div>
+            <button onClick={postData}>delete</button>
         </div>
     );
 };
--- src/Component/UpdateData/UpdateData.js
+++ src/Component/UpdateData/UpdateData.js
@@ -6,94 +6,117 @@ import axios from 'axios';
 
 
 const UpdateData = () => {
-    
-  const [imageUrl,setImageUrl]=useState()
-    const [reload,setReload]=useState('')
-    const[name,setName]=useState('')
-    const[version,setVersion]=useState('')
-    const[feature,setFeature]=useState('')
-    const[website,setWebsite]=useState('')
-    const[image,setImage]=useState('')
-   const history= useHistory()
 
-    const { register, handleSubmit, formState: { errors } } = useForm({});
+    const [dataInput, setDataInput] = useState([])
+    
+//   const [imageUrl,setImageUrl]=useState()
+//     const [reload,setReload]=useState('')
+//     const[name,setName]=useState('')
+//     const[version,setVersion]=useState('')
+//     const[feature,setFeature]=useState('')
+//     const[website,setWebsite]=useState('')
+//     const[image,setImage]=useState('')
+//    const history= useHistory()
+
+//     const { register, handleSubmit, formState: { errors } } = useForm({});
 
     
    
 const {id}=useParams()
 console.log(id)
-useEffect(()=>{
-    fetch(`https://guarded-woodland-52046.herokuapp.com/singleValue/${id}`)
-.then(res=>res.json())
-.then(data=>{
-    setName(data.event.name)
-    setFeature(data.event.feature)
-   setImage(data.event.image)
-    setWebsite(data.event.website)
-    setVersion(data.event.version)
+// useEffect(()=>{
+//     fetch(`https://guarded-woodland-52046.herokuapp.com/singleValue/${id}`)
+// .then(res=>res.json())
+// .then(data=>{
+//     setName(data.event.name)
+//     setFeature(data.event.feature)
+//    setImage(data.event.image)
+//     setWebsite(data.event.website)
+//     setVersion(data.event.version)
 
+// })
+
+
+
+// },[id,reload])
+
+
+    
+    const res = async () => {
+
+        const result = await axios.get('https://guarded-woodland-52046.herokuapp.com/data')
+        console.log(result.data)
+        setDataInput(result.data)
+
+
+    }
+
+useEffect(()=>{
+res()
 })
 
-
-
-},[id,reload])
-
-//update data
-
-const onSubmit = (data,e) => {
-   
- 
-    
     const updateData={
-        name:data.name,
-        version:data.version,
-        feature:data.feature,
-        website:data.website,
-        image:imageUrl
+        themeName:dataInput.event.themeName,
+        fork:dataInput.event.fork,
+        star:dataInput.event.star,
+       create:dataInput.event.create,
+        LastCommit:dataInput.event.LastCommit
+       
         }
-        console.log(updateData)
-    console.log(id)
+
+           setInterval(()=>{
     fetch(`https://guarded-woodland-52046.herokuapp.com/updateOne/${id}`,{
         method:'PATCH',
         headers:{'content-type':'application/json'},
         body: JSON.stringify(updateData)
-    })
-    .then(res=>{console.log(res)
+    },)},[1000])
+
+//update data
+
+// const onSubmit = (data,e) => {
+   
+ 
     
-      history.push('/getData')
-    })
-    .then(data=>setReload(data))
-    e.target.reset()
 
-};
-const handleImageChange=e=>{
+//         console.log(updateData)
+//     console.log(id)
 
-    const imageData=new FormData()
-    imageData.set('key','ac14fb7fe7d3b9b39f81a751405dbb8e')
-    imageData.append('image',e.target.files[0])
-    axios.post('https://api.imgbb.com/1/upload',imageData)
-    .then(res=>{
-        setImageUrl(res.data.data.display_url)
-    })
-}
+//    })
+//     .then(res=>{console.log(res)
+    
+//       history.push('/getData')
+//     })
+//     .then(data=>setReload(data))
+//     e.target.reset()
+
+// };
+// const handleImageChange=e=>{
+
+//     const imageData=new FormData()
+//     imageData.set('key','ac14fb7fe7d3b9b39f81a751405dbb8e')
+//     imageData.append('image',e.target.files[0])
+//     axios.post('https://api.imgbb.com/1/upload',imageData)
+//     .then(res=>{
+//         setImageUrl(res.data.data.display_url)
+//     })
+// }
 
    
     return (
         <div className="add-form">
             <h2 className="heading">Update Data</h2>
-           
+{/*            
            <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
+      
      <div className="input-group">
      <input className="form-control"  {...register("name")} placeholder="Theme Name" id="name" type="text" value={name}
       onInput={e=>setName(e.target.value)} /><br/>
      </div>
-      
-      {/* include validation with required or other standard HTML validation rules */}
+
       <div className="input-group">
      <input className="form-control" {...register("version")} placeholder="Theme Version" id="version" value={version} onInput={e=>setVersion(e.target.value)}/><br/>
      </div>
-      {/* errors will return when field validation fails  */}
+    
       {errors.exampleRequired && <span>This field is required</span>}
       
       <div className="input-group">
@@ -101,27 +124,27 @@ const handleImageChange=e=>{
      <textarea className="form-control" {...register("feature")} 
       name="feature" placeholder="Feature" id="feature" value={feature}onInput={e=>setFeature(e.target.value)}/><br/>
      </div>
-      {/* errors will return when field validation fails  */}
+      
       {errors.exampleRequired && <span>This field is required</span>}
 
       <div className="input-group">
      <textarea className="form-control" {...register("website")} placeholder="Inspiration Site" id="website" value={website}onInput={e=>setWebsite(e.target.value)}/><br/>
      </div>
-      {/* errors will return when field validation fails  */}
+    
       {errors.exampleRequired && <span>This field is required</span>}
       <div className="input-group">
      <input className="form-control" {...register("image")} placeholder="Theme Image" id="image"  type="file" 
     value={image} alt="fff" onInput={
          (e)=>handleImageChange(e)}/><br/>
      </div>
-      {/* errors will return when field validation fails  */}
+      
       {errors.exampleRequired && <span>This field is required</span>}
       
       
  <div class="d-grid gap-2"> <button class="btn btn-primary"  type="submit">Update</button></div>
   
 
-    </form>
+    </form> */}
 
     
          
